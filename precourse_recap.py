7ee2a8ee7512211a7bb5578bb75e030e3b3ced93
Mathias Kerr <mathiasfkerr@gmail.com>
2023-01-09 15:55:43 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,28 @@
+name = "Mathias"
+
+def greeting():
+    print("Hello", name, "How are you today?")
+greeting()
+
+wages_per_hour = 10
+hours_worked = 40
+# calculate the salary with a 20% tax
+
+salary = wages_per_hour * hours_worked * 52 * 0.8
+print(salary)
+
+no_1 = 5
+no_2 = 78
+no_3 = 92
+
+total_1 = no_1 - no_3
+print(total_1)
+total_2 = no_2 / no_3
+print(total_2)
+
+print(type("mathias"))
+print(type(5))
+print(type(13.5))
+print(type(True))
+
+print(name.upper())
